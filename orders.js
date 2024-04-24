3756ceb7105cd93c84c460abcc934bbaae7a7dda
josephkungu <157381649+JKungu@users.noreply.github.com>
2024-04-24 07:40:05 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,58 @@
+const Orders = [
+    {
+        ProductName: "Hp Elitebook Core i5",
+        ProductNumber: "23455",
+        paymentStatus: "Due",
+        shipping: "Received"
+
+    },
+    {
+        ProductName: "Hp Elitebook Core i5",
+        ProductNumber: "23455",
+        paymentStatus: "Due",
+        shipping: "pending"
+
+    },
+    {
+        ProductName: "Hp Elitebook Core i5",
+        ProductNumber: "23455",
+        paymentStatus: "Due",
+        shipping: "Declined"
+
+    },
+    {
+        ProductName: "Hp Elitebook Core i5",
+        ProductNumber: "23455",
+        paymentStatus: "Due",
+        shipping: "pending"
+
+    },
+    {
+        ProductName: "Hp Elitebook Core i5",
+        ProductNumber: "23455",
+        paymentStatus: "Due",
+        shipping: "Received"
+
+    },
+    {
+        ProductName: "Hp Elitebook Core i5",
+        ProductNumber: "23455",
+        paymentStatus: "Due",
+        shipping: "pending"
+
+    },
+    {
+        ProductName: "Hp Elitebook Core i5",
+        ProductNumber: "23455",
+        paymentStatus: "Due",
+        shipping: "pending"
+
+    },
+    {
+        ProductName: "Hp Elitebook Core i5",
+        ProductNumber: "23455",
+        paymentStatus: "Due",
+        shipping: "Received"
+
+    },
+]
